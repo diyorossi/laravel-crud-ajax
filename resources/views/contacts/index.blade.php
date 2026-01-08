51a512af4f4f5ed94b77cd530fcecd3026bf576a
diyorossi <diyorossi@gmail.com>
2026-01-08 16:11:39 +0000

    fixfeat : update btn name.

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -83,7 +83,7 @@
                 <div class="d-flex justify-content-between align-items-center">
                     <h3 class="mb-0"><i class="fas fa-address-book"></i> Manajemen Data Kontak</h3>
                     <button type="button" class="btn btn-primary" id="btnAddContact">
-                        <i class="fas fa-plus"></i> Tambah Kontakz
+                        <i class="fas fa-plus"></i> Tambah Kontakzzz
                     </button>
                 </div>
             </div>
